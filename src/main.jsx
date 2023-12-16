370ecbd82c47693e4b291da921bc7cbe57cc1590
AKarode <adit.karode@gmail.com>
2023-12-15 22:12:59 -0500

    Created LoginSignupPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import LoginSignup from './Components/LoginSignupPage/LoginSignup.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoginSignup/>
   </React.StrictMode>,
+  
 )
