3426917033480c50cc6a24c43bfc52835f75d3e3
halagilbert@gmail.com <halagilbert@gmail.com>
2023-12-22 12:49:17 -0500

    userPage updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoginSignup from './Components/LoginSignupPage/LoginSignup.jsx'
-import Logo from './Components/Logo/Logo.jsx';
 import UserPage from './Components/UserPage/UserPage.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserPage/>
-    <Logo/>
-    <LoginSignup/>
   </React.StrictMode>,
-  
 )
