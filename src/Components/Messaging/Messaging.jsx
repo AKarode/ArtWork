5a26084577ce9448e9006f475bb5340254ffdc1e
stendeze <117954183+stendeze@users.noreply.github.com>
2023-12-31 17:06:36 -0500

    Update Messaging.jsx

--- src/Components/Messaging/Messaging.jsx
+++ src/Components/Messaging/Messaging.jsx
@@ -3,27 +3,43 @@ import './Messaging.css';
 import ChatList from './ChatList'; 
 
 function Chat() {
-  const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const [currentChat, setCurrentChat] = useState(null);
+
+  const openChat = (chat) => {
+    setCurrentChat(chat);
+  };
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    if (currentChat) {
+      const newChat = { ...currentChat };
+      newChat.messages.push({ 
+        content: input, 
+        timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+        sender: 'me'
+      });
+      setCurrentChat(newChat);
+    }
     setInput('');
   };
 
   return (
     <div className="app-container">
-      <ChatList /> {}
+      <ChatList openChat={openChat} />
       <div className="chat-container">
-        <h1 className="chat-title">Chat</h1>
+        <h1 className="chat-title">Chat {currentChat && `with ${currentChat.name}`}</h1>
         <form className="chat-form">
           <input className="chat-input" value={input} onChange={event => setInput(event.target.value)} />
           <button className="chat-button" type="submit" onClick={sendMessage}>Send Message</button>
         </form>
-        {messages.map((message, index) => (
-          <p key={index} className="chat-message">{message}</p>
-        ))}
+        <div className="messages-container">
+          {currentChat && currentChat.messages.map((message, index) => (
+            <div key={index} className={`chat-message ${message.sender}`}>
+              <p>{message.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
