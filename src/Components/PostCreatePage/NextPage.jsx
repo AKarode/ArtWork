adff7146f5cf34417c56120ac301981b260a8b0c
maffeboi <tung.mat@northeastern.edu>
2023-12-31 00:12:58 -0500

    Font updates and CSS updates

--- src/Components/PostCreatePage/NextPage.jsx
+++ src/Components/PostCreatePage/NextPage.jsx
@@ -9,7 +9,7 @@ const NextPage = () => {
     const navigate = useNavigate();
 
     const handleBack = () => {
-        navigate('/create-project');
+        navigate('/post/create-project');
     };
 
     const handleLocationChange = (e) => {
@@ -36,49 +36,53 @@ const NextPage = () => {
 
     };
 
-return (
-    <div className="details-container">
-      <h2>Give us more details</h2>
-        
-        <div className='location-container'>
-          <div className="location">
-            <h3>Location</h3>
-              <input type="text" placeholder="Add Address"/>
+    return (
+      <div className="details-container">
+          <h2>Give Us More Details!</h2>
+
+          <div className='location-container'>
+              <div className="location">
+                  <h3>Location:</h3>
+                  <input
+                      type="text"
+                      placeholder="Add Address"
+                      value={location}
+                      onChange={handleLocationChange}
+                  />
+              </div>
           </div>
-        </div>
 
-        <div className='calandar-container'>
-          <div className="calandar">
-            <h3>Dates</h3>
-              <label>Start Date:</label>
-              <input type="date" />
-              <label>End Date:</label>
-              <input type="date" />
+          <div className='calandar-container'>
+              <div className="calandar">
+                  <h3>Dates:</h3>
+                  <label>Start Date:</label>
+                  <input type="date" />
+                  <label>End Date:</label>
+                  <input type="date" />
+              </div>
           </div>
-        </div>
 
-        <div className='photo-upload-container'>
-          <div className="photoupload">
-            <h3>Photos</h3>
-              <input type="file" multiple />
-            </div>
-        </div>
-
-        <div className='file-upload-container'>
-          <div className="fileupload">
-            <h3>Upload Files</h3>
-            <input type="file" multiple onChange={(handleFilesChange)} />
-            <button>Upload</button>
+          <div className='photo-upload-container'>
+              <div className="photoupload">
+                  <h3>Photos:</h3>
+                  <input type="file" multiple onChange={handlePhotoChange} />
+              </div>
           </div>
-        </div>
 
-      <div className="button-group">
-        <button onClick={handleBack}>Back</button>
-        <button onClick={handleCancel}>Cancel</button>
-        <button onClick={handleSave}>Save</button>
-        <button onClick={handlePost}>Post</button>
+          <div className='file-upload-container'>
+              <div className="fileupload">
+                  <h3>Upload Files:</h3>
+                  <input type="file" multiple onChange={handleFilesChange} />
+              </div>
+          </div>
+
+          <div className="button-group">
+              <button className='back-button' onClick={handleBack}>Back</button>
+              <button className='cancel-button' onClick={handleCancel}>Cancel</button>
+              <button className='save-button' onClick={handleSave}>Save</button>
+              <button className='post-button' onClick={handlePost}>Post</button>
+          </div>
       </div>
-    </div>
   );
 };
 
